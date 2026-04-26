1c4cd2bd8e50fe5a1cbaa7e7384f1659d30c4be9
Karine <Karine@example.com>
2026-04-26 21:00:09 +0200

    Réajustement du fichier index

--- public/index.php
+++ public/index.php
@@ -1,33 +1,12 @@
 
 <?php
 
-echo "Je suis sur covoiturage2026";
-
-echo "<br>";
-
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-
-
-
-
 require __DIR__ . '/../vendor/autoload.php';
 
 
-use MongoDB\Client;
+
 use Router\Router;
 use Source\App;
-
-
-$client = new Client("mongodb://localhost:27017");
-$db = $client->covoiturage;
-
-echo "Connexion OK";
-
-echo "<br>";
-
-
-
 
 
 
